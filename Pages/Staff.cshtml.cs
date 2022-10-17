a4de89dd70ad781b75768d3ef17fb56bd3ab0e31
Luc Roghi <lcr0059@arastudent.ac.nz>
2022-10-17 22:10:24 +0000

    Fixed the pages that were empty

--- Pages/Staff.cshtml.cs
+++ Pages/Staff.cshtml.cs
@@ -1,2 +1,21 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AraVirtualTour.Pages;
+
+public class StaffModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public StaffModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
+
 
--- Pages/ToolsHelp.cshtml.cs
+++ Pages/ToolsHelp.cshtml.cs
@@ -1,2 +1,19 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
+namespace AraVirtualTour.Pages;
+
+public class ToolsHelpModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public ToolsHelpModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
