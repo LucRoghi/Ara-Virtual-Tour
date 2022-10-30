10636a775ccd8ea35b0a301e83b8cf22bec6164f
Luc Roghi <lcr0059@arastudent.ac.nz>
2022-10-30 07:20:21 +0000

    Added Models for the database to use

---
+++ Models/StaffModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AraVirtualTour
+{
+    public class StaffModel
+    {
+        public int id {get; set;}
+        public string Title {get; set;}
+        public string Name {get; set;}
+        public string Department {get; set;}
+    }
+}
