9ae8ba666232b13258803ae9686bfb3ddfa1f6c0
Luc Roghi <lcr0059@arastudent.ac.nz>
2022-10-30 05:41:09 +0000

    fixed the mysql db intergration

--- Models/AppContext.cs
+++ Models/AppContext.cs
@@ -8,9 +8,19 @@ namespace AraVirtualTour
 {
     public class AppContext : DbContext 
     {
-        public DbSet<UserModel> Users {get; set;}
+        protected readonly IConfiguration Configuration;
+
+        public AppContext(IConfiguration configuration) 
+        {
+            Configuration = configuration;
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlite(@"Data Source=aravirtualtour.db");
+        {
+            options.UseSqlite(Configuration.GetConnectionString("AppDB"));
+        }
+
+        public DbSet<UserModel> Users {get; set;}
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddCoreAdmin();
 
 var app = builder.Build();
 
@@ -13,7 +17,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
