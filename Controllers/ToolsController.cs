23309e389be8c4c4a2dec32c394bfeb46569bf9f
Luc Roghi <lcr0059@arastudent.ac.nz>
2022-11-01 00:55:23 +0000

    Populating the website with content

---
+++ Controllers/ToolsController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AraVirtualTour;
+using System.Diagnostics;
+using AraVirtualTour.Models;
+
+namespace AraVirtualTour.Controllers;
+
+public class ToolsController : Controller 
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly AppContext _context;
+
+    public ToolsController(ILogger<HomeController> logger, AppContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public IActionResult Moodle()
+    {
+        return View();
+    }
+
+    public IActionResult Myara()
+    {
+        return View();
+    }
+}
